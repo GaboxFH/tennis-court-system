7f6bd569b2286750585c58eae046512f40fa5790
onqw <tut@ufl.edu>
2021-01-31 03:35:50 -0500

    create relationship between users and reservations

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,10 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+
+    public function reservations() {
+        return $this->belongsToMany(Reservation::class);
+    }
     /**
      * The attributes that are mass assignable.
      *
