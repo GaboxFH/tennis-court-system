fac7424d8241275856244bf99450869bb98d7bf6
GaboxFH <gabefernandez@ufl.edu>
2021-02-04 01:44:32 -0500

    Added placeholder components for my reservations and admin panel pages

---
+++ resources/js/components/ReservationsComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">My Reservations</div>
+
+                    <div class="card-body">
+                        <p>I'm an example component.</p>
+
+                        <div v-if="$can('View All Users')" >This text only is seen if you have the right persomissions.</div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
