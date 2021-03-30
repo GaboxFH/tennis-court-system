5840f3ed238c5471c2dcb1430ca5e862f7013f93
noah <noahred16@users.noreply.github.com>
2021-03-30 00:59:01 -0400

    query research

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -19,6 +19,33 @@ class ReservationController extends Controller
         return Reservation::orderBy('created_at', 'DESC')->get();
     }
 
+
+    public function court_play($month)
+    {
+        
+    }
+    public function member_play($month)
+    {
+        // new Date().getMonth()
+        
+        // return Reservation::orderBy('created_at')->get();
+        // User::select('id','name')->get();
+        // return Reservation::get();
+        $user_info = User::select('id','name')->get();
+
+        return Reservation::where('start_datetime','>=','2021-03-01 00:00:00')
+                            ->where('start_datetime','<','2021-04-01 00:00:00')
+                            // ->join('Reservation','Reservation.user_id','=','Users.id')
+                            // ->groupBy('user_id');
+                            ->get();
+
+        // return Reservation::where('start_datetime','<=','2020-04-01')->get();
+                            // ->where('start_datetime','<','2020-04-11 00:00:00')
+                            
+
+        // return User::select('id','name')->leftJoin();
+    }
+
     public function reservation_users($res_id, $user_id)
     {        
         $res_host = "";
@@ -28,10 +55,8 @@ class ReservationController extends Controller
         if($participants){
             foreach($participants as $participant){
                 if($participant->user_id == $user_id){
-                    // $res_host = User::where('id', $participant->user_id)->value('name');
                     $res_host = $participant->user_id;
                 } else {
-                    // $res_participants[$i] = User::where('id', $participant->user_id)->value('name');
                     $res_participants[$i] = $participant->user_id;
                     $i++;
                 }
@@ -41,7 +66,6 @@ class ReservationController extends Controller
             'res_host' => $res_host,
             'res_participants' => $res_participants
         ]);
-        // return $res_users;
     }
 
 
@@ -108,87 +132,41 @@ class ReservationController extends Controller
      */
     public function update(Request $request)
     {
-        $existingItem = Reservation::find($request->item["id"]);
-        //return "whoop";
-
+        // id is not passed in for created events so we must use start and court for this button to work correctly 
+        if (!isset($request->item["id"])){
+            $existingItem = Reservation::where('start_datetime', $request->item["start"])->where('court', $request->item["category"])->first();
+        } else {
+            $existingItem = Reservation::find($request->item["id"]);
+        }
+        
         if($existingItem) {
             $dataIndex = 0;
             $formIndex = 0;
-            
-            $formParticipants = $request->item["ordered_participants_ids"];
-            $existingParticipants = Reservation_User::where('reservation_id', $request->item["id"])->orderBy('user_id')->get();
-            
-            // if(count($existingParticipants)==0){
-            //     $existingItem->user_id = 50;
-            //     // while($formIndex != count($formParticipants)){
-            //     //     Reservation_User::create([
-            //     //         'reservation_id' => $request->item["id"],
-            //     //         'user_id' => $formParticipants[$i]
-            //     //     ]);
-            //     //     $formIndex++;
-            //     // }
-            //     $existingItem->save();
-            // } else 
-            if($existingParticipants){
 
-                // $sum = $existingParticipants[0]->user_id;
-                // for($i=0; $i<count($existingParticipants); $i++){
-                //     Reservation_User::create([
-                //         'reservation_id' => $request->item["id"],
-                //         'user_id' => $existingParticipants[$i]->user_id
-                //     ]);
-                //     // $sum = $sum + $existingParticipants[0];
-                // }
-                // for($i=0; $i<count($formParticipants); $i++){
-                //     Reservation_User::create([
-                //         'reservation_id' => $request->item["id"],
-                //         'user_id' => $formParticipants[$i]
-                //     ]);
-                // }
-                
-                // $temp_test = Reservation_User::where('reservation_id', $request->item["id"])->select('user_id')->orderBy('user_id')->get();(73);
-                // $temp_test->delete();
-                // $existingParticipants[3]->delete();
+            $existingParticipants = Reservation_User::where('reservation_id', $existingItem->id)->orderBy('user_id')->get();
+            $formParticipants = $request->item["ordered_participants_ids"];
+            
+            if($existingParticipants){
                 // participant replacement algorithm for the Reservation_User table
                 while($dataIndex != count($existingParticipants) || $formIndex != count($formParticipants)){
                     if($formIndex == count($formParticipants)) {
-                    // if($existingParticipants[$dataIndex]->user_id < $formParticipants[$formIndex] || $formIndex == count($request->item["ordered_participants_ids"])){
-                    //     // delete where 'user_id', $existingParticipants[$dataIndex]->user_id
-                        // $existingItem = Reservation::find($id);
-
-                        // if($existingItem){
-                        //     $existingItem->delete();
-                        //     return "Item successfully deleted.";
-                        // }
-                        // $delete = Reservation_User::find($existingParticipants[$dataIndex]->user_id);
-                        // if($delete){
-                        //     $delete->delete();
-                        //     // return "Item successfully deleted.";
-                        // }
                         $existingParticipants[$dataIndex]->delete();
                         $dataIndex++;
                     }
-                    
                     else if($dataIndex == count($existingParticipants)) {
-                    // else if($existingParticipants[$dataIndex] > $formParticipants[$formIndex] || $dataIndex == count($existingParticipants)){
-                    //     // create Reservation_User
-                    //     //  'user_id', $existingParticipants[$dataIndex]->id
-                    //     //  'reservation_id', $request->item["id"]
                         Reservation_User::create([
-                            'reservation_id' => $request->item["id"],
+                            'reservation_id' => $existingItem->id,
                             'user_id' => $formParticipants[$formIndex]
                         ]);
                         $formIndex++;
                     }
                     else if($existingParticipants[$dataIndex]->user_id < $formParticipants[$formIndex]) {
-                        // delete
                         $existingParticipants[$dataIndex]->delete();
                         $dataIndex++;
                     }
                     else if($existingParticipants[$dataIndex]->user_id > $formParticipants[$formIndex]){
-                        // add
                         Reservation_User::create([
-                            'reservation_id' => $request->item["id"],
+                            'reservation_id' => $existingItem->id,
                             'user_id' => $formParticipants[$formIndex]
                         ]);
                         $formIndex++;
@@ -199,43 +177,18 @@ class ReservationController extends Controller
                     }
                 }
                 
-                
-                // for ($i = 0; $i < count($existingParticipants); $i++){
-
-                // }
-                // if(count($existingParticipants))
+                $existingItem->title = $request->item["title"];
                 $existingItem->method = $request->item["method"];
                 $existingItem->user_id = $request->item["host"];
-                // $existingItem->user_id = 14;
-                $existingItem->title = $request->item["title"];
-
                 $existingItem->num_of_members = count($request->item["ordered_participants_ids"]);
-                // $existingItem->num_of_members = 13;
-                $existingItem->num_of_guests = count($existingParticipants);
-
-    //             $existingItem->method = $request->item["method"];
-    //             $existingItem->start_datetime = $request->item["start"];
-    //             $existingItem->end_datetime = $request->item["end"];
-    //             $existingItem->court = $request->item["category"];
-    //             $existingItem->num_of_members = $request->item["num_of_members"];
-    //             $existingItem->num_of_guests = $request->item["num_of_guests"];
-    //             $existingItem->user_id = $request->item["user_id"];
+                $existingItem->num_of_guests = 0; //hard coded
                 $existingItem->save();
+                
+                return "Update complete";
             }
+            
         }
-
         return "Item not found.";
-
-        // Reservation::create([
-        //     'title' => $request->item["title"],
-        //     'method' => $request->item["method"],
-        //     'start_datetime' => $request->item["start"],
-        //     'end_datetime' => $request->item["end"],
-        //     'court' => $request->item["category"],
-        //     'num_of_members' => $request->item["num_of_members"],
-        //     'num_of_guests' => $request->item["num_of_guests"],
-        //     'user_id' => $request->item["user_id"],
-        // ]);
     }
 
     public function adminupdate(Request $request)
@@ -243,32 +196,7 @@ class ReservationController extends Controller
         $existingItem = Reservation::where('start_datetime', $request->item["start"])
                                     ->where('court', $request->item["category"])
                                     ->update(['end_datetime' => $request->item["end"]]);
-
-//         if($existingItem){
-// //            $existingItem->user_id = $request->item["user_id"];
-//             $existingItem->title = $request->item["title"];
-//             $existingItem->method = $request->item["method"];
-//             $existingItem->start_datetime = $request->item["start"];
-//             $existingItem->end_datetime = $request->item["end"];
-//             $existingItem->court = $request->item["category"];
-//             $existingItem->num_of_members = $request->item["num_of_members"];
-//             $existingItem->num_of_guests = $request->item["num_of_guests"];
-//             $existingItem->user_id = $request->item["user_id"];
-//             $existingItem->save();
-//         }
-
-        return "Item not found.";
-
-        // Reservation::create([
-        //     'title' => $request->item["title"],
-        //     'method' => $request->item["method"],
-        //     'start_datetime' => $request->item["start"],
-        //     'end_datetime' => $request->item["end"],
-        //     'court' => $request->item["category"],
-        //     'num_of_members' => $request->item["num_of_members"],
-        //     'num_of_guests' => $request->item["num_of_guests"],
-        //     'user_id' => $request->item["user_id"],
-        // ]);
+        return "Event drag";
     }
 
     /**
